e873212af47a5029a822bd2134cab2383835b9e1
teawon <xodnjsvkvk9@gmail.com>
2022-07-12 20:37:33 +0900

    feat : Title Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Mainpage from "./pages/Mainpage";
+import UploadImagePage from "./pages/UploadImagePage";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainpage from "./pages/Mainpage";
 import AftLoginMainpage from "./pages/AftLoginMainpage";
 import ResultVideopage from "./pages/ResultVideopage";
+import "./App.css";
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
         <Route path="/upload" element={<Mainpage />} />
         <Route path="/signin" element={<AftLoginMainpage />} />
         <Route path="/result" element={<ResultVideopage />} />
+        <Route path="/upload" element={<UploadImagePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/UploadImagePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "components/Title";
+
+function UploadImagePage() {
+  return (
+    //실제 화면에 출력할 내용들 설정 해주는 곳
+
+    <Title textValue="Select portraits without mosaic"></Title>
+  );
+}
+
+export default UploadImagePage;
