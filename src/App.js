d4da539e0450330115c2a9174fa896265199310b
teawon <xodnjsvkvk9@gmail.com>
2022-07-12 20:37:33 +0900

    feat : Title Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Mainpage from "./pages/Mainpage";
+import UploadImagePage from "./pages/UploadImagePage";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Mainpage />} />
+        <Route path="/upload" element={<UploadImagePage />} />
       </Routes>
     </BrowserRouter>
   );
