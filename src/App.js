d1e72f6ec2b31c760fa0294a9428d0a087491fc6
조 성현 <agllwy116@tukorea.ac.kr>
2022-07-15 17:10:17 +0900

    feat : add mainpage , aftLoginmainpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainpage from "./pages/Mainpage";
+import AftLoginMainpage from "./pages/AftLoginMainpage";
+import ResultVideopage from "./pages/ResultVideopage";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/upload" element={<Mainpage />} />
+        <Route path="/signin" element={<AftLoginMainpage />} />
+        <Route path="/result" element={<ResultVideopage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/AftLoginMainpage.js
+++ src/pages/AftLoginMainpage.js
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "./Mainpage.module.css";
 import MainButton from "../components/MainButton";
 import OtherButton from "../components/OtherButton";
-function Mainpage() {
+import BasicButton from "../components/BasicButton";
+
+function AftLoginMainpage() {
   return (
     //실제 화면에 출력할 내용들 설정 해주는 곳
     <div className={styles.img_1}>
-      <nav className="static pt-40"></nav>
+      <nav className="static pt-40">
+        <BasicButton />
+      </nav>
       <section className={styles.section}>
         <div className={styles.For_Your}>
           For Your
@@ -21,14 +25,17 @@ function Mainpage() {
           <MainButton title="START" text="upload" />
         </div>
         <div>
-          <OtherButton title="SIGH IN" text="signin" />
+          <OtherButton title="PHOTO" text="photo" />
         </div>
         <div>
-          <OtherButton title="SIGH UP" text="signup" />
+          <OtherButton title="VIDEO" text="video" />
+        </div>
+        <div>
+          <OtherButton title="CHARACTER" text="character" />
         </div>
       </aside>
     </div>
   );
 }
 
-export default Mainpage;
+export default AftLoginMainpage;
