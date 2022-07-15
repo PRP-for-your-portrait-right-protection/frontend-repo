fe824fb2389a3a245cc579c296de0b4ca82333b8
teawon <xodnjsvkvk9@gmail.com>
2022-07-15 17:30:14 +0900

    chore : fix mergeError

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import Mainpage from "./pages/Mainpage";
 import UploadImagePage from "./pages/UploadImagePage";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Mainpage from "./pages/Mainpage";
 import AftLoginMainpage from "./pages/AftLoginMainpage";
 import ResultVideopage from "./pages/ResultVideopage";
@@ -14,8 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/people" element={<UploadImagePage />} />
-        <Route path="/people" element={<UploadImagePage />} />
-        <Route path="/upload" element={<Mainpage />} />
         <Route path="/signin" element={<AftLoginMainpage />} />
         <Route path="/result" element={<ResultVideopage />} />
         <Route path="/upload" element={<UploadImagePage />} />
