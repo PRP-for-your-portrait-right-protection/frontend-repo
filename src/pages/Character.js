6d8da187885384586bb3963533dd3b4ce4b65eff
minjilee <budlalswl1129@gamil.com>
2022-07-18 22:40:37 +0900

    chore: fix MainButton

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Character.css";
-import BasicButton from "../components/BasicButton2";
+import Button from "components/Button";
 import ListButton from "../components/ListButton";
-import CharacterResearch from "components/CharacterResearch";
 
 function Character() {
   return (
     <div>
-      <BasicButton></BasicButton>
+      <div className="absolute top-0 left-0 p-5 w-24 h-24">
+        <Button img="images\iconoir_home.png" url="/main"></Button>
+      </div>
+      <div className="absolute top-0 right-20 p-5 w-24 h-16">
+        <img src="images\admin.png" alt="userName" />
+      </div>
+      <div className="absolute top-0 right-0 p-5 w-28 h-16">
+        <Button img="images\signout.png" url="/"></Button>
+      </div>
       <ListButton></ListButton>
       <div className="font3">CHARACTER</div>
-      <CharacterResearch></CharacterResearch>
     </div>
   );
 }
