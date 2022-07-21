33226f43dc04e55e93002149be79bcf004b750b2
teawon <xodnjsvkvk9@gmail.com>
2022-07-21 11:56:46 +0900

    chore : add remark , make Token format

--- src/components/ImageListBlock.tsx
+++ src/components/ImageListBlock.tsx
@@ -22,13 +22,24 @@ function ImageListBlock() {
       },
     ],
   });
-  const [isLoding, setIsLoading]: [boolean, any] = useState(false);
+  const [isLoding, setIsLoading]: [boolean, any] = useState(false); //api통신 완료 상태 값
 
+  /**
+   * @name : Teawon
+   * @Function :fetchData - 특정 유저에게 등록된 모든 인물사진들을 가져와 설정하는 함수, 값이 정상적으로 설정되면 isLoading값을 true로 바꾼다
+   * @create-data: 2022-07-21
+   */
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        `https://1f413be8-5eb6-428f-a4d4-492745c03b38.mock.pstmn.io/userImage`
-      )
+      const result = await axios
+        .get(
+          `https://1f413be8-5eb6-428f-a4d4-492745c03b38.mock.pstmn.io/userImage`,
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        )
         .then(function (response) {
           let initialData = {
             //초기 설정 값
@@ -55,7 +66,7 @@ function ImageListBlock() {
             initialData.file = initialData.file.concat(imgListBlock);
           });
 
-          setTotalList(initialData);
+          setTotalList(initialData); //가져온 데이터의 가공한 최종 리스트를 totalList에 저장
           setIsLoading(true);
         })
         .catch(function (error) {
@@ -63,24 +74,30 @@ function ImageListBlock() {
           console.log(error);
         });
     };
-    fetchData();
+    if (localStorage.getItem("token") != null) {
+      fetchData();
+    } else {
+      setIsLoading(true); //만약 로그인이 되어있지 않다면 api를 보내지 않고 바로 로딩을 완료시킨다.
+    }
   }, []);
 
   /**
    * @name : Teawon
-   * @function :addImgList - 전체 ImgList의 개수를 늘리는 함수(컴포넌트 수 증가), 처음 이름은 other{count}로 지정하여 컴포넌트를 생성함
-   * @create-data: 2022-07-15
+   * @function :makeFormData - 사용자가 입력한 url들을 세션에 저장하고, 만약 파일이 있다면 backend로 보내 버킷에 저장된 Url로 가져와 세션에 이어서 저장한다.
+   * @create-data: 2022-07-21
    */
 
   const makeFormData = () => {
     const formData = new FormData();
 
-    let imageUrlList = [];
+    let imageUrlList = []; //AI에 입력될 이미지 url리스트
 
     totalList.file.forEach((element) => {
+      //리스트들의 이름을 키값으로 정의 후 각 이름값을 키값으로 하는 formData를 만든다.
       formData.append("name", element.name);
 
       element.pictures.forEach((list) => {
+        //만약 해당 리스트에 파일이 없고 모두 url이라면 리스트에 저장
         if (list.file == null) {
           imageUrlList.push(list.url); //url
         } else {
@@ -89,25 +106,10 @@ function ImageListBlock() {
       });
     });
 
-    for (let key of formData.keys()) {
-      console.log("FormData의 key를 확인합니다.");
-      console.log(key);
-    }
-
-    // FormData의 value 확인
-    for (let value of formData.values()) {
-      console.log("FormData의 Values를 확인합니다.");
-      console.log(value);
-    }
-
-    let array = formData.getAll("name");
-    console.log("파일이 업로드된 이름");
-    console.log(array);
+    let array = formData.getAll("name"); // 특정 이름을 키 값으로하는 데이터가 만약 Null이라면 삭제후 name의 배열값도 수정한다.
 
     totalList.file.forEach((element) => {
       if (formData.get(element.name) == null) {
-        console.log(element.name);
-        console.log("은 지금 파일이 없어서 지웁니다.");
         formData.delete(element.name);
         array = array.filter((data) => data !== element.name);
       }
@@ -118,29 +120,32 @@ function ImageListBlock() {
       formData.append("name", nameStr);
     });
 
-    console.log("파일이 업로드된 이름");
-    console.log(array);
-
-    console.log("배열값?");
-    console.log(imageUrlList);
-
-    for (let key of formData.keys()) {
-      console.log("FormData의 key를 확인합니다.");
-      console.log(key);
+    if (
+      sessionStorage.getItem("token") != null &&
+      formData.get("name") != null
+    ) {
+      //회원이고 , 동시에 보낼 파일 내용이 있다면 postApi보낸 후 응답값의 url들을 리스트에 추가
     }
 
-    // FormData의 value 확인
-    for (let value of formData.values()) {
-      console.log("FormData의 Values를 확인합니다.");
-      console.log(value);
-    }
+    sessionStorage.setItem("images", JSON.stringify(imageUrlList)); //
+    console.log(sessionStorage.getItem("images"));
+    // for (let key of formData.keys()) {
+    //   console.log("FormData의 key를 확인합니다.");
+    //   console.log(key);
+    // }
 
-    //  sessionStorage.removeItem("key");
-    // console.log(totalList);
-    // sessionStorage.setItem("key", JSON.stringify(totalList));
-    // console.log(sessionStorage.getItem("key"));
+    // // FormData의 value 확인
+    // for (let value of formData.values()) {
+    //   console.log("FormData의 Values를 확인합니다.");
+    //   console.log(value);
+    // }
   };
 
+  /**
+   * @name : Teawon
+   * @function :addImgList - 전체 ImgList의 개수를 늘리는 함수(컴포넌트 수 증가), 처음 이름은 other{count}로 지정하여 컴포넌트를 생성함
+   * @create-data: 2022-07-15
+   */
   const addImgList = (filename) => {
     window.scrollTo(0, document.body.scrollHeight);
     let strName = filename;
