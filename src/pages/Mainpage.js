fe11bf6977729b720ab2175804cfeea9f7afcb15
조 성현 <agllwy116@tukorea.ac.kr>
2022-07-26 17:55:00 +0900

    chore : edit Mainpage Css

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Mainpage.module.css";
-import MainButton from "../components/MainButton";
 import { Link } from "react-router-dom";
 function Mainpage() {
   const [token, setToken] = useState(false);
@@ -20,106 +19,69 @@ function Mainpage() {
     <>
       {token ? (
         <div className={styles.img_1}>
-          <nav className="static pt-40">
-            <div>
-              <img src="images\admin.png" className={styles.admin} />
-              <Link to="/">
-                <button
-                  className={styles.signOut}
-                  onClick={() => {
-                    localStorage.clear();
-                    setToken(false);
-                  }}
-                >
-                  <img src="images\signout.png" />
-                </button>
-              </Link>
-            </div>
-          </nav>
+          <div>
+            <img src="images\admin.png" className={styles.admin} />
+            <Link to="/">
+              <button
+                className={styles.signOut}
+                onClick={() => {
+                  localStorage.clear();
+                  setToken(false);
+                }}
+              >
+                <img src="images\signout.png" />
+              </button>
+            </Link>
+          </div>
           <div className={styles.section}>
             <div className={styles.For_Your_Login}>
               For Your
               <div className={styles.text_style_1}>PRP</div>
-            </div>
-            <div className={styles.portrait_right_protection_Login}>
-              portrait right protection
+              <div className={styles.portrait_right_protection_Login}>
+                portrait right protection
+              </div>
             </div>
           </div>
-          <aside className={styles.aside}>
-            <div>
-              <MainButton title="START" text="upload" />
-            </div>
-            <div>
-              <Link to={"/photo"}>
-                <button
-                  className="absolute top-1/3 right-0 w-64 h-20 mt-12 
-            text-3xl font-Stardos text-center text-black hover:text-white 
-            bg-inherit hover:bg-amber-900 border-2 border-amber-900"
-                >
-                  PHOTO
-                </button>
-              </Link>
-            </div>
-            <div>
-              <Link to={"/video"}>
-                <button
-                  className="absolute top-1/2 right-0 w-64 h-20 mt-12 
-            text-3xl font-Stardos text-center text-black hover:text-white 
-            bg-inherit hover:bg-amber-900 border-2 border-amber-900"
-                >
-                  VIDEO
-                </button>
-              </Link>
-            </div>
-            <div>
-              <Link to={"/character"}>
-                <button
-                  className="absolute top-2/3 right-0 w-64 h-20 mt-12 
-            text-3xl font-Stardos text-center text-black hover:text-white 
-            bg-inherit hover:bg-amber-900 border-2 border-amber-900"
-                >
-                  CHARACTER
-                </button>
-              </Link>
-            </div>
-          </aside>
+          <div>
+            <Link to={"/upload"}>
+              <button className={styles.button1}>START</button>
+            </Link>
+          </div>
+          <div>
+            <Link to={"/photo"}>
+              <button className={styles.button2}>PHOTO</button>
+            </Link>
+          </div>
+          <div>
+            <Link to={"/video"}>
+              <button className={styles.button3}>VIDEO</button>
+            </Link>
+          </div>
+          <div>
+            <Link to={"/character"}>
+              <button className={styles.button4}>CHARACTER</button>
+            </Link>
+          </div>
         </div>
       ) : (
         <div className={styles.img_1}>
-          <nav className="static pt-40"></nav>
           <div className={styles.section}>
             <div className={styles.For_Your_Login}>
               For Your
               <div className={styles.text_style_1}>PRP</div>
-            </div>
-            <div className={styles.portrait_right_protection_Login}>
-              portrait right protection
+              <div className={styles.portrait_right_protection_Login}>
+                portrait right protection
+              </div>
             </div>
           </div>
-          <aside className={styles.aside}>
-            <div>
-              <Link to={"/signin"}>
-                <button
-                  className="absolute top-40 right-0 w-64 h-20 mt-12 
-        text-3xl font-Stardos text-center text-black hover:text-white 
-        bg-inherit hover:bg-amber-900 border-2 border-amber-900"
-                >
-                  SIGH IN
-                </button>
-              </Link>
-            </div>
-            <div>
-              <Link to={"/signup"}>
-                <button
-                  className="absolute top-1/3 right-0 w-64 h-20 mt-12
-        text-3xl font-Stardos text-center text-black hover:text-white 
-        bg-inherit hover:bg-amber-900 border-2 border-amber-900"
-                >
-                  SIGH UP
-                </button>
-              </Link>
-            </div>
-          </aside>
+          <div>
+            <Link to={"/signin"}>
+              <button className={styles.button1}>SIGH IN</button>
+            </Link>
+            <Link to={"/signup"}>
+              <button className={styles.button2}>SIGH UP</button>
+            </Link>
+          </div>
         </div>
       )}
     </>
